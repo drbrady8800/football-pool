3f6d8827d1d7e24292e87a45288b8b61102c7079
Declan Brady <drbrady8800@gmail.com>
2024-12-27 17:11:46 -0800

    Add cors middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,22 @@
-export { auth as middleware } from '@/lib/auth';
+// export { auth as middleware } from '@/lib/auth';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const response = NextResponse.next();
+  
+  if (process.env.NODE_ENV === 'development') {
+    response.headers.set('Access-Control-Allow-Origin', 'http://localhost:3000');
+  }
+  if (process.env.NEXT_PUBLIC_VERCEL_URL) {
+    response.headers.set('Access-Control-Allow-Origin', process.env.NEXT_PUBLIC_VERCEL_URL);
+  }
+  response.headers.set('Access-Control-Allow-Origin', 'https://football-pool-rho.vercel.app');
+  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+  return response;
+}
 
 // Don't invoke Middleware on some paths
 export const config = {
